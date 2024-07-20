0d8ad32031215b5be7c4ec99687b029be06cfb19
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2024-07-20 16:39:12 +0300

    sprint1step1_1try

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/app-header/app-header';
+import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
+import ingredients from './data/ingredients.json';
+import BurgerConstructor from './components/burger-constructor/burger-constructor';
+
+interface Ingredient {
+  _id: string;
+  name: string;
+  image: string;
+  price: number;
+  type: string;
+}
 
 function App() {
+  const [selectedIngredients, setSelectedIngredients] = useState<Ingredient[]>([]);
+
+  const handleAddIngredient = (ingredient: Ingredient) => {
+    setSelectedIngredients(prevIngredients => {
+      if (ingredient.type === 'bun') {
+        const otherIngredients = prevIngredients.filter(ing => ing.type !== 'bun');
+        return [ingredient, ...otherIngredients];
+      } else {
+        const bun = prevIngredients.find(ing => ing.type === 'bun');
+        const otherIngredients = prevIngredients.filter(ing => ing.type !== 'bun');
+        return bun ? [bun, ...otherIngredients, ingredient] : [...prevIngredients, ingredient];
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <main style={{ display: 'flex', justifyContent: 'center', columnGap: '40px' }}>
+        <BurgerIngredients ingredients={ingredients} onAddIngredient={handleAddIngredient} />
+        <BurgerConstructor selectedIngredients={selectedIngredients} />
+      </main>
     </div>
   );
 }
